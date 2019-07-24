906bdf5ebea08b67215055bdba17a65cd973ae7a
MendFeld <53190630+MendFeld@users.noreply.github.com>
2019-07-23 17:17:34 -0700

    Add files via upload

---
+++ BarcodeGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+namespace BarcodeD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEncode_Click(object sender, EventArgs e)
+        {
+            BarcodeWriter writer = new BarcodeWriter() { Format = BarcodeFormat.CODE_128 };
+            pic.Image = writer.Write(txtDecode.Text);
+        }
+
+        private void BtnDecode_Click(object sender, EventArgs e)
+        {
+            BarcodeReader reader = new BarcodeReader();
+            var result = reader.Decode((Bitmap)pic.Image);
+            if (result != null)
+                txtDecode.Text = result.Text;
+        }
+    }
+}
